81105093b5905433ac99e4ba4685348c4f8946c9
Antoine <ravet.antoine.eln@gmail.com>
2018-02-27 23:35:21 +0100

    Création du front Application en node prêt à recevoir les bouchons du jar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
+import { HttpModule } from '@angular/http';
+import { MDBBootstrapModule } from './typescripts/free';
 import { AppComponent } from './app.component';
-import { routes } from './app.router';
-import { metaReducers, reducers } from './store';
-import { SharedModule } from './shared/shared.module';
-import { WeatherService } from './weather/weather.service';
-import { WeatherEffects } from './store/weather/weather.effects';
-import { FeedEffects } from './store/feed/feed.effects';
-import { ProfileEffects } from './store/profile/profile.effects';
-import { environment } from '../environments/environment';
+
+import { HomeComponent } from './home/home.component';
+import {MatSelectModule} from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    SharedModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
-    EffectsModule.forRoot([
-      ProfileEffects,
-      FeedEffects,
-      WeatherEffects
-    ]),
-    !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : [],
-    RouterModule.forRoot(
-      routes,
-      {
-        useHash: true
-      }
-    )
+    MatSelectModule,
+    MDBBootstrapModule.forRoot(),
+    HttpModule,
   ],
-  providers: [
-    WeatherService
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas:      [ NO_ERRORS_SCHEMA ]
 })
-export class AppModule {}
+export class AppModule { }
