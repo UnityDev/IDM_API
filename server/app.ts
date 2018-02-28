ccb764df7728c1b4ae09e2b91a223c56faeafd1c
Antoine <ravet.antoine.eln@gmail.com>
2018-02-28 16:55:26 +0100

    Prépation au back

--- server/app.ts
+++ server/app.ts
@@ -3,11 +3,8 @@ import * as compression from "compression";
 import * as express from "express";
 import * as path from "path";
 
-import { feedRouter } from "./routes/feed";
-import { loginRouter } from "./routes/login";
 import { protectedRouter } from "./routes/protected";
-import { publicRouter } from "./routes/public";
-import { userRouter } from "./routes/user";
+import { userRouter } from "./routes/video";
 
 const app: express.Application = express();
 
@@ -18,17 +15,7 @@ app.use(compression());
 app.use(urlencoded({ extended: true }));
 
 // api routes
-app.use("/api/secure", protectedRouter);
-app.use("/api/login", loginRouter);
-app.use("/api/public", publicRouter);
-app.use("/api/feed", feedRouter);
-app.use("/api/user", userRouter);
-
-if (app.get("env") === "production") {
-
-  // in production mode run application from dist folder
-  app.use(express.static(path.join(__dirname, "/../client")));
-}
+app.use("/api/video", protectedRouter);
 
 // catch 404 and forward to error handler
 app.use((req: express.Request, res: express.Response, next) => {
