4640a01f53695836788c812f87db012bb5ab1586
ALEXIS\Alexis <renaultalexis35@gmail.com>
2018-03-01 20:34:43 +0100

    change ressources path

--- server/routes/video.ts
+++ server/routes/video.ts
@@ -4,14 +4,14 @@ const userRouter: Router = Router();
 
 function createVideo() {
   var exec = require('child_process').exec;
-  var child = exec('java -jar idm_tp_jar.jar vignette',
+  var child = exec('java -jar server/ressources/idm_tp_jar.jar vignette',
     function (error, stdout, stderr){
       console.log('Output -> ' + stdout);
       if(error !== null){
         console.log("Error -> "+error);
       }
   });
-  var child = exec('java -jar idm_tp_jar.jar variante',
+  var child = exec('java -jar server/ressources/idm_tp_jar.jar variante',
     function (error, stdout, stderr){
       console.log('Output -> ' + stdout);
       if(error !== null){
