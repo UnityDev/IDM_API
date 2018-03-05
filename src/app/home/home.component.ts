a60d5888e86739b1c6498bdee0677867cf3ed18d
ALEXIS\Alexis <renaultalexis35@gmail.com>
2018-03-05 03:38:00 +0100

    add thumbnails in the template and fix videoCreation displaying bug

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HomeService} from './home.service';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -12,17 +13,34 @@ export class HomeComponent implements OnInit {
   mandatory: any = 'flute';
   optionnal: any;
   alternatives: any;
+  videoDisplayed: any;
+  srcAlternatives: any;
+  @ViewChild('videoPlayer') videoplayer: any;
 
   ngOnInit() {
     this.alternatives = 'fonte';
     this.createVideoRandom();
+    this.videoDisplayed = 'http://localhost:4300/videosCreated/videoRandom.mp4';
+    this.srcAlternatives = 'http://localhost:4300/thumbnails/alternative-thumbnailv31.jpg';
   }
 
   private createVideoRandom() {
     this.homeservice.createVideo();
   }
 
-  addVariante() {
+  addVariante(event: any) {
     this.homeservice.addVariante(this.mandatory, this.optionnal, this.alternatives);
+    setTimeout(() => this.videoDisplayed = 'http://localhost:4300/videosCreated/videoCreate.mp4', 2000);
+    setTimeout(() =>this.videoplayer.nativeElement.load(), 2000);
+    setTimeout(() =>this.videoplayer.nativeElement.play(), 2000);
+  }
+
+  updateThumbnail() {
+    if(this.alternatives == 'fonte') {
+      this.srcAlternatives = 'http://localhost:4300/thumbnails/alternative-thumbnailv31.jpg';
+    } else {
+      this.srcAlternatives = 'http://localhost:4300/thumbnails/alternative-thumbnailv32.jpg';
+    }
+    
   }
 }
